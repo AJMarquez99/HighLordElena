d126a039f34345a970aabbb855cca80caec3a5b2
Alejandro Marquez <alejandromarquez@live.com>
2019-01-04 23:20:14 -0500

    Added a hidden post page for admin

---
+++ post.php
@@ -0,0 +1,33 @@
+<?php
+  $page_title = "Create Post";
+  include ('components/header.html');
+?>
+<div class="col">
+  <h2>Make New Post</h2>
+  <p>
+    Want to make a new post, fill out the form below.
+  </p>
+  <form class="blog-posting" action="post.php" method="post">
+    <fieldset>
+      <div class="input-blocks">
+        <label for="title">Title:</label>
+        <input type="text" name="title" id="title" value="" required />
+      </div>
+      <div class="input-blocks">
+        <label for="media">Image/Video:</label>
+        <input type="text" name="media" id="media" value="" />
+      </div>
+      <div class="input-blocks">
+        <label for="desc">Description:</label>
+        <input type="text" name="media" id="media" value="" />
+      </div>
+      <textarea name="content" id="content" placeholder="Thoughts?"></textarea>
+      <div class="input-blocks submit-button">
+        <input type="submit" name="submit" id="submit" value="submit" />
+      </div>
+    </fieldset>
+  </form>
+</div>
+<?php
+  include ('components/footer.html');
+?>
