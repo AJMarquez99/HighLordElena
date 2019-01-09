76ec02c58f6422903822be4fa4e71fef86a5932a
Alejandro Marquez <alejandromarquez@live.com>
2019-01-08 19:43:13 -0500

    Finished About Page

--- about.php
+++ about.php
@@ -2,30 +2,34 @@
   $page_title = "About";
   include ('components/_header.html');
 ?>
-<div class="col">
-  <div class="container">
+<div class="col about">
+  <div class="container mt-md-5">
     <div class="row">
-      <div class="col">
-        <img src="images/about_overview.jpg" alt="Picure of me">
+      <div class="col-md-6 img-container">
+        <div class="img-container">
+          <img src="images/about_overview.jpg" alt="Picure of me" />
+        </div>
       </div>
-      <div class="col">
-        <h3>Overview</h3>
+      <div class="col-md-6">
+        <h3>Who Am I</h3>
         <p>Is branched in my up strictly remember. Songs but chief has ham widow downs. Genius or so up vanity cannot. Large do tried going about water defer by. Silent son man she wished mother. Distrusts allowance do knowledge eagerness assurance additions to. <br />Attachment apartments in delightful by motionless it no. And now she burst sir learn total. Hearing hearted shewing own ask. Solicitude uncommonly use her motionless not collecting age. The properly servants required mistaken outlived bed and. Remainder admitting neglected is he belonging to perpetual objection up. Has widen too you decay begin which asked equal any. </p>
       </div>
     </div>
-    <div class="row">
-        <div class="col">
+    <div class="row mt-md-5 mb-md-5">
+        <div class="col mt-md-5 mb-md-5">
           <h3>Art Style</h3>
           <p>Performed suspicion in certainty so frankness by attention pretended. Newspaper or in tolerably education enjoyment. Extremity excellent certainty discourse sincerity no he so resembled. Joy house worse arise total boy but. Elderly up chicken do at feeling is. Like seen drew no make fond at on rent. Behaviour extremely her explained situation yet september gentleman are who. Is thought or pointed hearing he.</p>
         </div>
     </div>
-    <div class="row">
-        <div class="col">
+    <div class="row mb-md-5">
+        <div class="col-md-6">
           <h3>Where I Want to Go</h3>
           <p>Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration. An mr settling occasion insisted distance ladyship so. Not attention say frankness intention out dashwoods now curiosity. Stronger ecstatic as no judgment daughter speedily thoughts. Worse downs nor might she court did nay forth these.</p>
         </div>
-        <div class="col">
-          <img src="images/about_future.jpg" alt="Me on a motorbike">
+        <div class="col-md-6 img-container">
+          <div class="img-container">
+            <img src="images/about_future.jpg" alt="Me on a motorbike" />
+          </div>
         </div>
     </div>
   </div>
