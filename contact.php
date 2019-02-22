279524756df68b9b4c90e7138bf65beec1486487
Alejandro Marquez <alejandromarquez@live.com>
2019-02-21 19:06:09 -0500

    (AMarquez - M) Created a pencil-like contact form, might change it so made it a new branch

--- contact.php
+++ contact.php
@@ -6,22 +6,24 @@
   <div class="row mb-5 flex-column">
     <h3>Interested in some of the artwork or want your own personal piece, let me know by filling out the for below.</h3>
     <form>
-      <div class="col">
-        <div class="input-block">
-          <label>Name</label>
-          <input type="text" name="name" id="name" value="Name" />
+      <div class="row justify-content-around">
+        <div class="col-4">
+          <div class="input-block">
+            <label>Name:</label>
+            <input type="text" name="c_name" id="c_name" autocomplete="new-password"/>
+          </div>
+          <div class="input-block">
+            <label>Phone Number:</label>
+            <input type="text" name="c_phone_number" id="c_phone_number" autocomplete="new-password"/>
+          </div>
+          <div class="input-block">
+            <label>Email:</label>
+            <input type="email" name="c_email" id="c_email" autocomplete="new-password" />
+          </div>
         </div>
-        <div class="input-block">
-          <label>Phone Number</label>
-          <input type="email" name="email" id="email" value="Number" />
+        <div class="col-7 d-flex justify-content-center">
+          <textarea name="content" id="content" placeholder="Whatever you'd like to say."></textarea>
         </div>
-        <div class="input-block">
-          <label>Email</label>
-          <input type="text" name="email" id="email" value="Email" />
-        </div>
-      </div>
-      <div class="col">
-        <textarea name="content" id="content" placeholder="Whatever you'd like to say."></textarea>
       </div>
     </form>
   </div>
