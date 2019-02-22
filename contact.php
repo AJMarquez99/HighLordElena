5a27b3a28f062069f399ef94fddaaa3d4bcd574a
Alejandro Marquez <alejandromarquez@live.com>
2019-02-21 19:41:28 -0500

    (AMarquez - M) Rounded sleek contact form, animation to section display

--- contact.php
+++ contact.php
@@ -6,22 +6,24 @@
   <div class="row mb-5 flex-column">
     <h3>Interested in some of the artwork or want your own personal piece, let me know by filling out the for below.</h3>
     <form>
-      <div class="col">
-        <div class="input-block">
-          <label>Name</label>
-          <input type="text" name="name" id="name" value="Name" />
+      <div class="row">
+        <div class="col-4">
+          <div class="input-block">
+            <label>Name:</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div class="input-block">
+            <label>Phone Number:</label>
+            <input type="email" name="email" id="email" />
+          </div>
+          <div class="input-block">
+            <label>Email:</label>
+            <input type="text" name="email" id="email" />
+          </div>
         </div>
-        <div class="input-block">
-          <label>Phone Number</label>
-          <input type="email" name="email" id="email" value="Number" />
+        <div class="col-8 form-message">
+          <textarea name="content" id="content" placeholder="Whatever you'd like to say."></textarea>
         </div>
-        <div class="input-block">
-          <label>Email</label>
-          <input type="text" name="email" id="email" value="Email" />
-        </div>
-      </div>
-      <div class="col">
-        <textarea name="content" id="content" placeholder="Whatever you'd like to say."></textarea>
       </div>
     </form>
   </div>
